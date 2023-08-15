cc9315f0b441aa3fe9f1b0c8cc84415f82b7f4b9
309 <309@academicos.cesde.edu.co>
2023-08-15 08:37:37 -0500

    se reciben datos por teclado y se castea

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,6 +1,6 @@
 #comentario linea
 
-nombreUsuario="Natalia Andrea Mafla Salgado"
+'''nombreUsuario="Natalia Andrea Mafla Salgado"
 edadUsuario=23
 estaturaUsuario=1.63
 esHinchaDelVerde=True
@@ -10,3 +10,14 @@ print(f"Su nombre es:  {nombreUsuario} y tiene {edadUsuario} años.")
 comidasFavoritas=["ajiaco", "arroz con leche", "pollo"]
 print(comidasFavoritas)
 print(comidasFavoritas[2])
+
+#Entradas por teclado: 
+lugarTrabajoUsuario=input("Digita el lugar de trabajo: ")
+print(f"usted trabaja en: {lugarTrabajoUsuario}")'''
+
+#Pedimos dos numeros
+
+numeroUno=int(input("Digita un numero: "))
+numeroDos=int(input("Digita otro numero: "))
+sumatoria=numeroUno+numeroDos
+print(f"la suma de {numeroUno}+{numeroDos}={sumatoria}")
