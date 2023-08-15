045f407aa7f599bd80debbc7aceda7c4089bf2fe
309 <309@academicos.cesde.edu.co>
2023-08-15 08:06:40 -0500

    feat:creamos variables primimtivas y lista de datos

---
+++ ejemplo1.py
@@ -0,0 +1,12 @@
+#comentario linea
+
+nombreUsuario="Natalia Andrea Mafla Salgado"
+edadUsuario=23
+estaturaUsuario=1.63
+esHinchaDelVerde=True
+
+print(f"Su nombre es:  {nombreUsuario} y tiene {edadUsuario} años.")
+
+comidasFavoritas=["ajiaco", "arroz con leche", "pollo"]
+print(comidasFavoritas)
+print(comidasFavoritas[2])
